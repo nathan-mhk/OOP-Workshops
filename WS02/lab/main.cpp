fc23742fc816d458a89ca672619bea06482e3962
Nathan <mhk.nathan@gmail.com>
2024-01-25 12:26:18 -0500

    Implemented Part1 code

--- WS02/lab/main.cpp
+++ WS02/lab/main.cpp
@@ -13,6 +13,16 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+
+/**
+ * Name: Nathan Kong
+ * Email: nkong@myseneca.ca
+ * ID: 150950236
+ * Date: 2024-01-25
+ * 
+ * I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
