07b1665fe45091d16e9600aa0cbe2f39ad8d5ab4
Nathan <mhk.nathan@gmail.com>
2024-02-06 13:37:09 -0500

    WS04 Part 2 Init

--- WS04/DIY/Guitar.cpp
+++ WS04/DIY/Guitar.cpp
@@ -13,6 +13,16 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+
+/**
+ * Name: Nathan Kong
+ * Email: nkong@myseneca.ca
+ * ID: 150950236
+ * Date: 2024-02-06
+ * 
+ * I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
@@ -32,4 +42,123 @@ namespace seneca{
       strncpy(m_material, ma, MAT_LEN);
       m_gauge = ga;
    };
+
+   /**
+    * Initializes the current object to an empty state except
+    * for the model name which will be initialized by the
+    * value of the parameter. The parameter will also have
+    * a default value of "Stratocaster".
+   */
+   Guitar::Guitar(const char* mod = "Stratocaster") {
+
+   }
+
+   /**
+    * The three-argument constructor initializes the data
+    * members based on the provided parameters:
+    * 
+    * - `strs` is an array of GuitarStr that will be used to
+    *   populate the dynamic array.
+    * - `ns` is the size of the array argument (`strs`)
+    * - `mod` is a model name for the Guitar
+    * 
+    * If the parameters are not valid the current object is
+    * set to an empty state.
+   */
+   Guitar::Guitar(GuitarStr strs[], int ns, const char* mod) {
+
+   }
+
+   /**
+    * The destructor deallocates any resources stored by
+    * the current object.
+   */
+   Guitar::~Guitar() {
+
+   }
+
+   /**
+    * This function changes a GuitarStr on the Guitar by
+    * passing one in through the parameter `gs`. The `sn`
+    * parameter refers to the index in the array (`m_strings`)
+    * as to which GuitarStr is going to be changed. If the
+    * provided `sn` value is an invalid index for the array
+    * then no string change occurs.
+    * 
+    * This function returns true if a change occurred and
+    * false otherwise.
+   */
+   bool Guitar::changeString(const GuitarStr& gs, int sn) {
+      return true;
+   }
+
+   /**
+    * This function restrings the Guitar with a new set of
+    * GuitarStr provided through the parameters (an array of
+    * GuitarStr and an integer representing the size of that
+    * array). Take note to carefully manage any
+    * allocation/deallocation of dynamic memory when replacing
+    * the GuitarStr array.
+   */
+   void Guitar::reString(GuitarStr strs[], int ns) {
+
+   }
+
+   /**
+    * This function will de-string the Guitar of all of its
+    * GuitarStr. This means to deallocate all resources
+    * currently held by the Guitar and to then set the
+    * m_numStrings to an empty state.
+   */
+   void Guitar::deString() {
+
+   }
+
+   /**
+    * This function will return true if the Guitar has
+    * GuitarStr and false otherwise.
+   */
+   bool Guitar::strung() const {
+      return true;
+   }
+
+   /**
+    * This function will return true if any GuitarStr on the
+    * Guitar has a matching gauge value to the provided
+    * parameter and false otherwise.
+   */
+   bool Guitar::matchGauge(double ga) const {
+      return true;
+   }
+
+   /**
+    * This function will display the details of a Guitar to
+    * the ostream via the parameter os.
+    * 
+    * If the Guitar is in an empty state print out the
+    * following:
+    * 
+    * ***Empty Guitar***<newline>
+    * 
+    * Otherwise, print out the Guitar details as follows:
+    * 
+    * Guitar Model: [m_model]<newline>
+    * Strings: [m_numStrings]<newline>
+    * 
+    * If the Guitar is strung/has GuitarStr then print out
+    * each string using this format:
+    * 
+    * #1 [string1.m_material] | [string1.gauge]<newline>
+    * #2 [string2.m_material] | [string2.gauge]<newline>
+    * ...
+    * 
+    * For the GuitarStr material use a field width of
+    * MAT_LEN. For the gauge use 1 decimal place precision.
+    * 
+    * Return the parameter os at the end of this function
+    * regardless of what is printed out.
+   */
+   std::ostream& Guitar::display(std::ostream& os) const {
+      return os;
+   }
 }
--- WS04/DIY/Guitar.h
+++ WS04/DIY/Guitar.h
@@ -13,6 +13,16 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+
+/**
+ * Name: Nathan Kong
+ * Email: nkong@myseneca.ca
+ * ID: 150950236
+ * Date: 2024-02-06
+ * 
+ * I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #ifndef SENECA_GUITAR_H
 #define SENECA_GUITAR_H
@@ -22,6 +32,7 @@
 namespace seneca{
 
   const int MAT_LEN = 10;
+  const int NAME_LEN = 15;
 
   class GuitarStr {
      char m_material[MAT_LEN]{};
@@ -33,6 +44,113 @@ namespace seneca{
      GuitarStr(const char* ma, double ga);
   };
 
+   class Guitar {
+      GuitarStr* m_strings;
+      int m_numStrings;
+      char m_model[NAME_LEN];
+
+   public:
+      /**
+       * Initializes the current object to an empty state except
+       * for the model name which will be initialized by the
+       * value of the parameter. The parameter will also have
+       * a default value of "Stratocaster".
+      */
+      Guitar(const char* mod = "Stratocaster");
+
+      /**
+       * The three-argument constructor initializes the data
+       * members based on the provided parameters:
+       * 
+       * - `strs` is an array of GuitarStr that will be used to
+       *   populate the dynamic array.
+       * - `ns` is the size of the array argument (`strs`)
+       * - `mod` is a model name for the Guitar
+       * 
+       * If the parameters are not valid the current object is
+       * set to an empty state.
+      */
+      Guitar(GuitarStr strs[], int ns, const char* mod);
+
+      /**
+       * The destructor deallocates any resources stored by
+       * the current object.
+      */
+      ~Guitar();
+
+      /**
+       * This function changes a GuitarStr on the Guitar by
+       * passing one in through the parameter `gs`. The `sn`
+       * parameter refers to the index in the array (`m_strings`)
+       * as to which GuitarStr is going to be changed. If the
+       * provided `sn` value is an invalid index for the array
+       * then no string change occurs.
+       * 
+       * This function returns true if a change occurred and
+       * false otherwise.
+      */
+      bool changeString(const GuitarStr& gs, int sn);
+
+      /**
+       * This function restrings the Guitar with a new set of
+       * GuitarStr provided through the parameters (an array of
+       * GuitarStr and an integer representing the size of that
+       * array). Take note to carefully manage any
+       * allocation/deallocation of dynamic memory when replacing
+       * the GuitarStr array.
+      */
+      void reString(GuitarStr strs[], int ns);
+
+      /**
+       * This function will de-string the Guitar of all of its
+       * GuitarStr. This means to deallocate all resources
+       * currently held by the Guitar and to then set the
+       * m_numStrings to an empty state.
+      */
+      void deString();
+
+      /**
+       * This function will return true if the Guitar has
+       * GuitarStr and false otherwise.
+      */
+      bool strung() const;
+
+      /**
+       * This function will return true if any GuitarStr on the
+       * Guitar has a matching gauge value to the provided
+       * parameter and false otherwise.
+      */
+      bool matchGauge(double ga) const;
+
+      /**
+       * This function will display the details of a Guitar to
+       * the ostream via the parameter os.
+       * 
+       * If the Guitar is in an empty state print out the
+       * following:
+       * 
+       * ***Empty Guitar***<newline>
+       * 
+       * Otherwise, print out the Guitar details as follows:
+       * 
+       * Guitar Model: [m_model]<newline>
+       * Strings: [m_numStrings]<newline>
+       * 
+       * If the Guitar is strung/has GuitarStr then print out
+       * each string using this format:
+       * 
+       * #1 [string1.m_material] | [string1.gauge]<newline>
+       * #2 [string2.m_material] | [string2.gauge]<newline>
+       * ...
+       * 
+       * For the GuitarStr material use a field width of
+       * MAT_LEN. For the gauge use 1 decimal place precision.
+       * 
+       * Return the parameter os at the end of this function
+       * regardless of what is printed out.
+      */
+      std::ostream& display(std::ostream& os = std::cout) const;
+   };
 }
 
 #endif
