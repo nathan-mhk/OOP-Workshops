de22446e26b46ef3b1f3547be89772c8bbf986e5
Nathan Kong <mhk.nathan@gmail.com>
2024-03-22 18:54:40 -0400

    WS09 Skeleton

---
+++ WS09/lab/Book.cpp
@@ -0,0 +1,12 @@
+/**
+ * Name: Nathan Kong
+ * Email: nkong@myseneca.ca
+ * ID: 150950236
+ * Date: 2024-03-22
+ * 
+ * I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
+#include "Book.h"
+namespace seneca {
+}
